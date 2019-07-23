7e9ce39802a41accd156bb6155d027546829a380
kjsu <surtn@naver.com>
2019-07-23 12:49:15 +0900

    jinsu test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class App extends React.Component {
     render() {
         return (
-            <h1>This is HOT!</h1>
+            <h1>jinsu branch</h1>
         )
     }
 }
