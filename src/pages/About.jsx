e09459710c86b0787042c3deb5c94e66f20749bc
Vinckel <newskys@naver.com>
2019-08-04 01:48:47 +0900

    v4

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,16 +3,23 @@ import axios from 'axios';
 import Header from './Header';
 
 class About extends React.Component {
+    state = {
+        data: '',
+    };
+
     componentDidMount() {
-    //     axios.get('https://jsonplaceholder.typicode.com/todos/1')
-    //     .then((res) => console.log(res));
+        axios.get('https://jsonplaceholder.typicode.com/todos/1')
+        .then((res) => this.setState({ data: res.data, }))
+        .catch(err => console.log(err));
     }
 
     render() {
+        console.log('{this.state.data}', this.state.data);
         return (
             <div>
                 <Header />
                 name param: {this.props.match.params.name}
+                
             </div>
         )
     }
