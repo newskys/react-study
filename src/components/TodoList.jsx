2a34645306a16e77e030d6ba5f405a9a9a225962
kjsu <surtn@naver.com>
2019-08-23 17:35:30 +0900

    [modified] 멤버 이름과 해당 멤버의 데이터 가져오기

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,16 +7,40 @@ import axios from 'axios';
 class TodoList extends React.Component {
     state = {
         list: [],
+        name: null
     };
 
+    componentDidMount() {
+        
+        // 멤버 이름 가져오기
+        axios.get(`https://tuhon.herokuapp.com/reactstudy/member`)
+        .then((res) => {
+            console.log('member_name', res.data.memberName);
+
+            this.setState({
+                name: res.data.memberName
+            })
+
+            // 해당 멤버의 데이터 가져오기
+            axios.get(`https://tuhon.herokuapp.com/reactstudy/${res.data.memberName}/todo`)
+            .then((res) => {
+                console.log('member_data', res.data);
+
+                this.setState({
+                    list: res.data
+                })
+            });
+        });
+    }
+
     render() {
-        const { list } = this.state;
+        const { list, name } = this.state;
         console.log('list', list);
 
         return (
             <main className="todo-list-template">
                 <div className="title">
-                    의 오늘 할 일
+                    {name}의 오늘 할 일
                 </div>
                 <section className="todos-wrapper">
                     <TodoItemList list={list} />
