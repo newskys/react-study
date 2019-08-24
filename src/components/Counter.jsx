cecabb35acd6b0ae719df09b6a19186b3c1bc1da
Kyusik Shin <newskys@naver.com>
2019-08-25 02:18:18 +0900

    week5-complete

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Counter.css';
 
+import { connect } from 'react-redux';
+import { increment, decrement } from '../redux/reducers/count';
+
 class Counter extends React.Component {
     componentDidMount() {
     }
 
     render() {
+        const { count, increment, decrement } = this.props;
+        console.log('count', count.count);
         return (
             <main className="todo-list-template">
                 <div className="title">
@@ -13,9 +18,9 @@ class Counter extends React.Component {
                 </div>
                 <section className="form-wrapper">
                     <div className="form">
-                        <input readOnly={true} value={0} />
-                        <div className="create-button">증가</div>
-                        <div className="create-button">감소</div>
+                        <input readOnly={true} value={count.count} />
+                        <div className="create-button" onClick={increment}>증가</div>
+                        <div className="create-button" onClick={decrement}>감소</div>
                     </div>
                 </section>
             </main>
@@ -23,4 +28,26 @@ class Counter extends React.Component {
     }
 }
 
-export default Counter;
+const mapStateToProps = (state) => ({
+    count: state.count,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    increment: () => dispatch(increment()),
+    decrement: () => dispatch(decrement()),
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(Counter);
+
+// export default connect(
+//     (state) => ({
+//         count: state.count,
+//     }),
+//     {
+//         increment,
+//         decrement,
+//     },
+// )(Counter);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createStore } from 'redux';
+import reducers from './redux/reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducers);
+
 ReactDOM.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 , document.getElementById('root'));
