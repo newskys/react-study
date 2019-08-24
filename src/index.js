d894c1f60e3df56e0fe774ee9e8c37267a3d3f12
Kyusik Shin <newskys@naver.com>
2019-08-25 02:12:23 +0900

    week5

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import { createStore } from 'redux';
-import reducers from './redux/reducers';
-import { Provider } from 'react-redux';
-
-const store = createStore(reducers);
-
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+    <App />
 , document.getElementById('root'));
