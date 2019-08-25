86c42c0d569213b649b1c9d2eadf16fae2c94240
Kyusik Shin <kyusik.shin@navercorp.com>
2019-08-25 18:25:57 +0900

    week5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-// import TodoList from './components/TodoList';
 import Counter from './components/Counter';
 
 export default class App extends React.Component {
     render() {
         return (
             <>
-                {/* <TodoList /> */}
                 <Counter />
             </>
         )
