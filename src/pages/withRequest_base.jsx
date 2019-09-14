121c5a976dfdf62fe3c8a71bff104382fcc080d9
kjsu <surtn@naver.com>
2019-09-15 02:31:16 +0900

    withRequest_base.jsx add

---
+++ src/pages/withRequest_base.jsx
@@ -0,0 +1,14 @@
+// import React, { Component } from 'react';
+
+// const withRequest = (url) => (WrappedComponent) => {
+//     return class extends Component {
+
+//         render() {
+//             return (
+//                 <WrappedComponent {...this.props}/>
+//             )
+//         }
+//     }
+// }
+
+// export default withRequest;
